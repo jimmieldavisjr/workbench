09908fb0cfe47f80f8a98f563e69de548f73b2b6
Jimmie Davis <life@jimmieldavisjr.com>
2026-05-26 15:14:56 -0700

    Add Tailwind CSS v4 setup to React TypeScript workbench training project

--- React/Training/yt-mosh-react-2023/src/App.tsx
+++ React/Training/yt-mosh-react-2023/src/App.tsx
@@ -1,7 +1,13 @@
-import Message from './Message'
+import Message from "./Message";
 
 function App() {
-  return <div><Message/></div>
+  return (
+    <div className="flex min-h-screen block items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-blue-500">
+        <Message />
+      </h1>
+    </div>
+  );
 }
 
 export default App;
